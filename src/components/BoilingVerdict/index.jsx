473770c6b055988ca58e9eaaefd9709299dec36b
WuZhiJian <1347609251@qq.com>
2021-02-02 11:47:01 +0800

    React状态提升-摄氏度与华氏度转换

--- src/components/BoilingVerdict/index.jsx
+++ src/components/BoilingVerdict/index.jsx
@@ -10,38 +10,53 @@ function toCelsius(fahrenheit) {
 function toFahrenheit(celsius) {
     return (celsius * 9 / 5) + 32;
 }
+
+function tryConvert(temperature, convert) {
+    const input = parseInt(temperature);
+    if (Number.isNaN(input)) {
+        return ''
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded;
+}
+
 class Calculator extends React.Component {
     constructor(props) {
         super(props);
-        this.OnTemperature = this.OnTemperature.bind(this);
+        this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+        this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
         this.state = {
-            temperature: '', 
+            temperature: '',
             scale: 'c'
         }
     }
 
-    handleCelsiusChange(value){
-        this.setState({temperature: value, scale: 'c'});
+    handleCelsiusChange(value) {
+        this.setState({ temperature: value, scale: 'c' });
     }
 
-    handleFahrenheitChange(value){
-        this.setState({temperature: value, scale: 'f'});
+    handleFahrenheitChange(value) {
+        this.setState({ temperature: value, scale: 'f' });
     }
 
     render() {
-        let scale = this.props.scale;
+        const scale = this.state.scale;
+        const temperature = this.state.temperature;
+        const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+        const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
         return (
             <div>
                 <Temperature
-                 scale='c' 
-                 temperature={this.state.temperature}
-                 OnTemperature={this.OnTemperature}
-                 />
+                    scale='c'
+                    temperature={celsius}
+                    OnTemperature={this.handleCelsiusChange}
+                />
                 <Temperature
-                 scale='f' 
-                 temperature={this.state.temperature}
-                 OnTemperature={this.OnTemperature}
-                 />
+                    scale='f'
+                    temperature={fahrenheit}
+                    OnTemperature={this.handleFahrenheitChange}
+                />
             </div>
         )
     }
